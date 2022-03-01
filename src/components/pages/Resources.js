c06f3f2b71887c3c748a57130f6dc0b46eaa9a09
manuelmort <manuelmort@gmail.com>
2022-02-28 22:59:29 -0800

    updated resources page

--- src/components/pages/Resources.js
+++ src/components/pages/Resources.js
@@ -13,24 +13,19 @@ export default class Resources extends React.Component {
                         <div class="card-body">
                             <h5 class="card-title text-white">CPE</h5>
                            
-                             <p class="card-text text-white">Computer Engineering Channels</p>
+                             <p class="card-text text-white">Computer Engineering Texts</p>
+                             <p class="card-text"><small class="text-muted">Last updated: 2/28/2022</small></p>
+
                             <div className="text-center">
                                 <div class="btn-group">
                                     <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                         CPE channels
                                     </button>
                                     <div class="dropdown-menu">
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/408002665361047552">CPE-64</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/668950211900473384">CPE-138</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/436273658605076500">CPE-142</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/668941382441762817">CPE-144</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/384839984495984641">CPE-166</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/438425736060207114">CPE-151</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/435892425421226004">CPE-159</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/668941366063267861">CPE-153</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/383355486021353472">CPE-185</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/487308091822440458">CPE-186</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/652297368296751119">CPE-187</a>
+
+                                        <a class="dropdown-item" href="https://cdn.discordapp.com/attachments/751264154916225084/752257965918257293/The_Morgan_Kaufmann_Series_in_Computer_Architecture_and_Design_David_A._Patterson_John_L._Hennessy_-.pdf">CPE-142</a>
+                                        <a class="dropdown-item" href="https://cdn.discordapp.com/attachments/751264154916225084/752255242590224455/intel_book.pdf">CPE-185</a>
+
                                         
                                     </div>
                                 </div>
@@ -40,8 +35,8 @@ export default class Resources extends React.Component {
                     <div class="card bg-dark">
                         <div class="card-body">
                             <h5 class="card-title text-white">CSC</h5>
-                            <p class="card-text text-white">Computer Science Channels</p>
-                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            <p class="card-text text-white">Computer Science Texts</p>
+                            <p class="card-text"><small class="text-muted">Last updated: 2/28/2022</small></p>
 
                             <div className ="text-center">
                                 <div class="btn-group  m-3 row"   style={{minWidth:"100px"}}>
@@ -49,12 +44,7 @@ export default class Resources extends React.Component {
                                             PRE-CSC
                                         </button>
                                         <div class="dropdown-menu row">
-                                            <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/489151065825214474">CSC-15</a>
-                                            <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/382411886295121920">CSC-20</a>
-                                            <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/751137399886839928">CSC-25</a>
-                                            <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/496193010518851594">CSC-28</a>
-                                            <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/372536207965683723">CSC-35</a>
-                                            <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/363078631855226881">CSC-60</a>
+                                            <a class="dropdown-item" href="https://cdn.discordapp.com/attachments/751264154916225084/948108008985092126/Building_Java_Programs_A_Back_to_Basics_Approach_5th_Edition_by_Stuart_Reges_Marty_Stepp_z-lib.org.pdf">CSC-15,CSC-20</a>
                                             
                                             
                                         </div>
@@ -64,14 +54,10 @@ export default class Resources extends React.Component {
                                         CSC
                                     </button>
                                     <div class="dropdown-menu">
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/375445533193535488">CSC-130</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/406145604893409280">CSC-131</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/412519916600688642">CSC-133</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/408141633926725635">CSC-134</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/406145403881521164">CSC-135</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/398222102039101442">CSC-137</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/406146094045593601">CSC-138</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/572518298537820160">CSC-139</a>
+                                        <a class="dropdown-item" href="https://cdn.discordapp.com/attachments/751264154916225084/752254566577471598/Algorithhms_4th_Edition_by_Robert_Sedgewick_Kevin_Wayne.pdf">CSC-130</a>
+                                        <a class="dropdown-item" href="https://cdn.discordapp.com/attachments/751264154916225084/751541604443095100/Fundamentals_of_Database_Systems_7th_Edition_by_Ramez_Elmasri_Shamkant_B._Navathe.pdf">CSC-137</a>
+                                        <a class="dropdown-item" href="https://cdn.discordapp.com/attachments/751264154916225084/751283872523747389/James_Kurose_Keith_Ross_-_Computer_Networking__A_Top-Down_Approach_7th_Edition.pdf">CSC-138</a>
+                                        <a class="dropdown-item" href="https://cdn.discordapp.com/attachments/751264154916225084/751284155697987654/operating-system-concepts-10th.pdf">CSC-139</a>
                                         
                                         
                                     </div>
@@ -83,8 +69,8 @@ export default class Resources extends React.Component {
                     <div class="card bg-dark">
                         <div class="card-body">
                         <h5 class="card-title text-white">EEE</h5>
-                        <p class="card-text text-white">Electrical and Electronic Engineering Channels</p>
-                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        <p class="card-text text-white">Electrical and Electronic Engineering Texts</p>
+                        <p class="card-text"><small class="text-muted">Last updated: 2/28/2022</small></p>
 
                         <div className ="text-center">
                             <div class="btn-group">
@@ -92,37 +78,11 @@ export default class Resources extends React.Component {
                                         EEE channels
                                     </button>
                                     <div class="dropdown-menu">
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/668947170086354954">EEE-64</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/385926926893711371">EEE-108</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/438437162560651285">EEE-109</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/668960109623509013">EEE-110</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/406592279801757696">EEE-117</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/668949368396709888">EEE-120</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/668957225796239410">EEE-122</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/410707031876698112">EEE-130</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/668961433794641983">EEE-135</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/505078071028744192">EEE-141</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/668959710682021900">EEE-142</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/759633778334236722">EEE-143</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/668961538823946242">EEE-144</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/668961454086422549">EEE-136</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/668961557971206154">EEE-145</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/668961574035128350">EEE-146</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/548365674892754965">EEE-161</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/668960663422369842">EEE-162</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/668960694053502976">EEE-165</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/668960130523725844">EEE-166</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/668949565591781396">EEE-174</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/668960404415709215">EEE-178</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/418536156679110666">EEE-180</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/668957257287073808">EEE-181</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/668960730841874442">EEE-183</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/417157268711407620">EEE-184</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/417460268222775316">EEE-185</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/494693576689582104">EEE-187</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/668960417682423808">EEE-188</a>
-                                        <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/671562668686966792">EEE-196a</a>
-                                        
+                                        <a class="dropdown-item" href="https://cdn.discordapp.com/attachments/751264154916225084/752255057210114118/Microelectronic_7thed_Sedra.pdf">EEE-108</a>
+                                        <a class="dropdown-item" href="https://cdn.discordapp.com/attachments/751264154916225084/948108404801540136/James_W._Nilsson_Susan_A._Riedel_-_Electric_Circuits_11th_Edition_2018_Pearson_-_libgen.lc.pdf">EEE-117</a>
+                                        <a class="dropdown-item" href="https://cdn.discordapp.com/attachments/751264154916225084/753380151580033115/B._P._Lathi_-_Signal_Processing_and_Linear_Systems_-Oxford_University_Press_USA_2000_Bookmarked.pdf">EEE-180</a>
+                                        <a class="dropdown-item" href="https://cdn.discordapp.com/attachments/751264154916225084/753380183461068980/Norman_S._Nise_-_Control_Systems_Engineering-Wiley_2015.pdf">EEE-184</a>
+
                                     </div>
                                 </div>  
                             </div>                      
@@ -134,26 +94,14 @@ export default class Resources extends React.Component {
                     <div class="card bg-dark">
                         <div class="card-body">
                         <h5 class="card-title text-white">ENGR</h5>
-                        <p class="card-text text-white">General Engineering Channels</p>
-                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        <p class="card-text text-white">General Engineering Texts</p>
+                        <p class="card-text"><small class="text-muted">Last updated: 2/28/2022</small></p>
                         <div class="btn-group">
                                 <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                     ENGR Channels
                                 </button>
                                 <div class="dropdown-menu">
-                                    <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/668925594347503637">ENGR-1</a>
-                                    <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/413109624774983691">ENGR-17</a>
-                                    <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/413110156138643456">ENGR-30</a>
-                                    <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/415216567022911488">ENGR-50</a>
-                                    <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/413109759584239617">ENGR-45</a>
-                                    <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/415216601919389706">ENGR-112</a>
-                                    <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/495328646190465034">ENGR-110</a>
-                                    <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/476112539982364672">ENGR-115</a>
-                                    <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/417460763540717568">ENGR-120</a>
-                                    <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/493871582444257317">ENGR-124</a>
-                                    <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/668925751411867668">ENGR-132</a>
-                                    <a class="dropdown-item" href="https://discordapp.com/channels/355086293501214730/417158276401070080">ENGR-140</a>
-                                    
+                                <a class="dropdown-item" href="https://cdn.discordapp.com/attachments/751264154916225084/948108404801540136/James_W._Nilsson_Susan_A._Riedel_-_Electric_Circuits_11th_Edition_2018_Pearson_-_libgen.lc.pdf">ENGR-17</a>            
                                 </div>
                             </div>                        
                         </div>
