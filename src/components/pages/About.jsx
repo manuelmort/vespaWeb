fe5fb3cc1ec4651a010529ff51eccf3aba2aa79b
Manuel Morteo <manuelmort@Manuels-MacBook-Pro.local>
2022-03-09 20:49:57 -0800

    updating officer pictures

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -22,10 +22,12 @@ class Home extends React.Component {
                             <div className="col-sm p-3">
                                 
                                 <div class="card" style={{width: "15rem"}}>
-                                    <img class="card-img-top" src="juan.jpg" alt="Card image cap"></img>
+                                    <img class="card-img-top" src="jeff.jpg" alt="Card image cap" style={{minHeight:"275px"}}></img>
                                     <div class="card-body">
-                                        <h5 class="card-title">President</h5>
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <h5 class="card-title">Founder of Vespa</h5>
+                                        <h6 class="card-text">Geff Freire</h6>
+                                        <p class="card-text">Status: Graduate</p>
+                                        <p className ="card-text">Degree: B.S. in Computer Engineering</p>
                                         <a href="#" class="btn btn-primary">Go somewhere</a>
                                     </div>
                                 </div>
@@ -33,25 +35,32 @@ class Home extends React.Component {
                             <div className="col-sm p-3">
                                 
                                 <div class="card" style={{width: "15rem"}}>
-                                    <img class="card-img-top" src="juan.jpg" alt="Card image cap"></img>
+                                    <img class="card-img-top" src="lonnie1.png" alt="Card image cap" style={{minHeight:"275px"}}></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">On-Campus President</h5>
+                                        <h6 class="card-text">Lonathan Elijouan Spann Jr.</h6>
+                                        <p class="card-text">Status: Senior Year</p>
+                                        <p className ="card-text">Major: Computer Engineering</p>
+                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-sm p-3">
+                                
+                                <div class="card" style={{width: "15rem"}}>
+                                    <img class="card-img-top" src="juan.jpg" alt="Card image cap" style={{maxHeight:"300px"}}></img>
                                     <div class="card-body">
                                         <h5 class="card-title">Vice president</h5>
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <h6 class="card-text">Juan Carlos Gonzales</h6>
+                                        <p class="card-text">Status: Junior Year</p>
+ 
+                                        <p class="card-text"> Major: Civil Engineering</p>
+
                                         <a href="#" class="btn btn-primary">Go somewhere</a>
                                     </div>
                                 </div>
                             </div>
-                            <div className="col-sm p-3">
-                                
-                                <div class="card" style={{width: "15rem"}}>
-                                    <img class="card-img-top" src="juan.jpg" alt="Card image cap"></img>
-                                    <div class="card-body">
-                                        <h5 class="card-title">Secretary</h5>
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                                    </div>
-                                </div>
-                            </div>
+                           
                             <div className="col-sm p-3">
                                 
                                 <div class="card" style={{width: "15rem"}}>
